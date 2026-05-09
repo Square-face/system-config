6be5c968c8516c10621e62ab4b60cb0c0f759043
Linus Michelsson <linus@sq8.dev>
2026-05-09 05:27:12 +0200

    fix: brought shrexbox config up to parity with laptop

--- desktop.nix
+++ desktop.nix
@@ -2,17 +2,27 @@ nixpkgs: name: system: nixpkgs.lib.nixosSystem {
   inherit system;
 
   modules = [
-    { networking.hostName = name; }
+    {
+      networking.hostName = name;
+      hardware.graphics.enable = true;
+      security.pam.services.swaylock= {};
+      hardware.bluetooth.enable = true;
+    }
 
     ./hosts/${name}.nix
-    ./hosts/${name}-hardware.nix
 
     ./modules/system/systemd-boot.nix
     ./modules/system/networking.nix
+    ./modules/system/locale.nix
+    ./modules/system/nixos.nix
+    ./modules/system/nh.nix
+
     ./modules/system/sq8.nix
 
     ./modules/services/containers.nix
     ./modules/services/pipewire.nix
+    ./modules/services/kerberos.nix
+    ./modules/services/upower.nix
     ./modules/services/sshd.nix
 
     ./modules/programs/man.nix
