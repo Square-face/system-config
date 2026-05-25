40afee67b4a5af92011839827a3b084cd481e57f
Linus Michelsson <linus@sq8.dev>
2026-05-25 22:24:19 +0200

    style: stopped using classes
    Moved all the modules into flake.nix instead of having intermediary
    <class>.nix files.

--- flake.nix
+++ flake.nix
@@ -6,32 +6,84 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.11";
   };
 
-  outputs = {nixpkgs-unstable, nixpkgs-stable, ...}: {nixosConfigurations = {
-    shrexbox = import ./desktop.nix nixpkgs-unstable "shrexbox" "x86_64-linux";
+  outputs = {nixpkgs-unstable, nixpkgs-stable, ...}: {
+    nixosConfigurations = {
+      shrexbox = nixpkgs-unstable.lib.nixosSystem {
+        system = "x86_64-linux";
 
-    flappy = import ./laptop.nix nixpkgs-unstable "flappy" "x86_64-linux";
+        modules = [
+          ./hosts/shrexbox.nix
+          ./modules/system/sq8.nix
 
-    frank = nixpkgs-stable.lib.nixosSystem {
-      system = "x86_64-linux";
+          ./modules/system/systemd-boot.nix
+          ./modules/system/networking.nix
+          ./modules/system/bluetooth.nix
+          ./modules/system/graphics.nix
+          ./modules/system/locale.nix
+          ./modules/system/nixos.nix
+          ./modules/system/nh.nix
 
-      modules = [
-        ./hosts/frank.nix
+          ./modules/services/containers.nix
+          ./modules/services/pipewire.nix
+          ./modules/services/kerberos.nix
+          ./modules/services/upower.nix
+          ./modules/services/sshd.nix
 
-        ./modules/system/systemd-boot.nix
-        ./modules/system/networking.nix
-        ./modules/system/nixos.nix
-        ./modules/system/nh.nix
-        ./modules/system/rootbash.nix
+          ./modules/programs/man.nix
+          ./modules/programs/zsh.nix
+          ./modules/programs/steam.nix
+        ];
+      };
 
-        ./modules/system/sq8.nix
+      flappy = nixpkgs-unstable.lib.nixosSystem {
+        system = "x86_64-linux";
 
-        ./modules/services/containers.nix
-        ./modules/services/dnsmasq.nix
-        ./modules/services/sshd.nix
+        modules = [
+          ./hosts/flappy.nix
+          ./modules/system/sq8.nix
 
-        ./modules/programs/man.nix
-        ./modules/programs/zsh.nix
-      ];
+          ./modules/system/systemd-boot.nix
+          ./modules/system/networking.nix
+          ./modules/system/bluetooth.nix
+          ./modules/system/graphics.nix
+          ./modules/system/locale.nix
+          ./modules/system/nixos.nix
+          ./modules/system/nh.nix
+          ./modules/system/tlp.nix
+
+          ./modules/services/containers.nix
+          ./modules/services/pipewire.nix
+          ./modules/services/kerberos.nix
+          ./modules/services/upower.nix
+          ./modules/services/sshd.nix
+
+          ./modules/programs/man.nix
+          ./modules/programs/zsh.nix
+        ];
+
+      };
+
+      frank = nixpkgs-stable.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./hosts/frank.nix
+          ./modules/system/sq8.nix
+
+          ./modules/system/systemd-boot.nix
+          ./modules/system/networking.nix
+          ./modules/system/rootbash.nix
+          ./modules/system/nixos.nix
+          ./modules/system/nh.nix
+
+          ./modules/services/containers.nix
+          ./modules/services/dnsmasq.nix
+          ./modules/services/sshd.nix
+
+          ./modules/programs/man.nix
+          ./modules/programs/zsh.nix
+        ];
+      };
     };
-  };};
+  };
 }
