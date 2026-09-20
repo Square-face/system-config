ef85c8f7bfa5738b5382a18c0f6ab4b09b411dc4
Linus Michelsson <linus@sq8.dev>
2026-09-20 05:04:20 +0200

    fix(frank/dns): prevent service name leak

--- flappy/default.nix
+++ flappy/default.nix
@@ -6,6 +6,7 @@
   obs.enable = true;
 
   shitcloud.vpn.enable = true;
+  shitcloud.vpn.dns = false; # Prevent dns server from exposing internal service names
   dns.enable = true;
   dns.DoH = true;
 
